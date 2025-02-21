001014125ff1a9b1d38bbf0e53e917ae469f4e94
Shyam-723 <shyam.gupta0723@gmail.com>
2025-02-20 17:19:03 -0800

    feat/Updated all code to react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './components/Header';
-import BodyContainer from './components/BodyContainer';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import BodyContainer from './components/BodyContainer/BodyContainer';
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
--- src/components/BodyContainer/BodyContainer.js
+++ src/components/BodyContainer/BodyContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './BodyContainer.css';
 import {ScrollContainer, Animator, ScrollPage, batch, Sticky, Fade, MoveOut} from "react-scroll-motion";
 
 function BodyContainer() {
@@ -7,34 +8,24 @@ function BodyContainer() {
 
             <ScrollContainer>
                 <ScrollPage>
-                    <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
-                        <span className="animation-text">
+                    <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -1000))}>
+                        <div className="animation-text">
                             Hello! Welcome to my webpage! Scroll down to know more about me, or click on `Projects`
                             to see some of my work.
-                        </span>
+                        </div>
+
+                        <div className="space">
+                        </div>
+
+                        <div className='animation-text'>
+                            I am a 3rd year CSE Student at UC Merced. I am a member of the student-run HackMerced Team
+                            and a Chancellor Scholar recipient.
+                        </div>
                     </Animator>
-
-
                 </ScrollPage>
             </ScrollContainer>
 
-            {/*<img*/}
-            {/*    src={`${process.env.PUBLIC_URL}/images/my-image.jpg`}*/}
-            {/*    alt="Me! Shyam Gupta!"*/}
-            {/*    className="body-image"*/}
-            {/*/>*/}
-            {/*<div className="body-text-box">*/}
-            {/*    <p>*/}
-            {/*        I am a 3rd year CSE Student at UC Merced. I am a member of the*/}
-            {/*        student-run HackMerced Team and a Chancellor Scholar recipient!*/}
-            {/*    </p>*/}
-            {/*    <p>I have experience with Java, C++, HTML, CSS and JavaScript.</p>*/}
-            {/*    <p>*/}
-            {/*        I'm also interested in Software Engineering, Machine Learning and*/}
-            {/*        Data Science!*/}
-            {/*    </p>*/}
-            {/*</div>*/}
-        </div>
+        </div> // End of file
     );
 }
 
