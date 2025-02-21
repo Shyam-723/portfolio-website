177f8f57a831d7ae5b3f232324dc45d417bc002a
Shyam-723 <shyam.gupta0723@gmail.com>
2025-02-20 23:07:17 -0800

    feat/Added scrolling sections

--- src/components/BodyContainer/BodyContainer.js
+++ src/components/BodyContainer/BodyContainer.js
@@ -8,18 +8,32 @@ function BodyContainer() {
 
             <ScrollContainer>
                 <ScrollPage>
-                    <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -1000))}>
-                        <div className="animation-text">
-                            Hello! Welcome to my webpage! Scroll down to know more about me, or click on `Projects`
-                            to see some of my work.
-                        </div>
+                    <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -500))}>
+                        <div className = "scrolling-section"> 
+                            <div className="animation-text">
+                                Hello! Welcome to my webpage! Scroll down to know more about me, or click on `Projects`
+                                to see some of my work.
+                            </div>
 
-                        <div className="space">
+                            <div className="loading-image">
+                            <img src={`${process.env.PUBLIC_URL}/images/my-image.jpg`} alt="Me" />
+                            </div>    
                         </div>
+                    </Animator>
+                </ScrollPage>
 
-                        <div className='animation-text'>
-                            I am a 3rd year CSE Student at UC Merced. I am a member of the student-run HackMerced Team
-                            and a Chancellor Scholar recipient.
+                <ScrollPage>
+                    <Animator animation={batch(Fade(), MoveOut(0, -500))}>
+                        <div className="scrolling-section">
+                            <div className='animation-text'> 
+                                <h1>About Me</h1>
+                                <p>
+                                    I am a 3rd-year Computer Science student at UC Merced. 
+                                    I am a member of the student-run HackMerced Team and a Chancellor Scholar recipient.
+                                </p>
+                            </div>
+
+                            
                         </div>
                     </Animator>
                 </ScrollPage>
